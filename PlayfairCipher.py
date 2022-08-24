ddce6beb7fbcd1c1f4a456421078482c117bbef1
veronicaglh <veronicagt.IT@gmail.com>
2022-08-24 22:53:33 +0300

    first commit

---
+++ PlayfairCipher.py
@@ -0,0 +1,7 @@
+# This program will be able to receive a plain text and encrypt it using the playfair cipher
+# This program will also be able to decrypt a cipher text back into the original message
+# In addition it will also have a UI that is built with tkinter.
+# For this program to work some packages will need to be installed.
+# To install these packages run the following commands on your IDE's terminal
+# pip install pillow
+
