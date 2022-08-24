f43893756b9acd8ecc7e547abedbcc06f57166a1
veronicaglh <veronicagt.IT@gmail.com>
2022-08-24 23:47:30 +0300

    add function to decrypt cipher text

--- PlayfairCipher.py
+++ PlayfairCipher.py
@@ -37,14 +37,14 @@ title3.place(x=470,y=210)
 title4.place(x=440,y=240)
 userKey.place(x=510, y=210)
 userText.place(x=510, y=240)
-myWindow.mainloop()
 
 
 # Part II - COMPUTATION
 
 # function to encrypt text
 def encrypt():
-    #userKey.get() will return the value the user enters as a key
+    # userKey.get() will return the value the user enters as a key
+    # userKey is the name of the entry field that has been defined above in PART I
     key = userKey.get()
     key = key.replace(" ", "")
     key = key.upper()
@@ -91,7 +91,8 @@ def encrypt():
                     return loc
     ###################
 
-    # userText.get() will return the value the user enters as a plain text
+    # userText.get() will return the value the user enters as a plain
+    # It will get the value from the input field named userText which has been defined in PART I as a tkinter entry widget
     msg=userText.get()
     msg=msg.upper()
     msg=msg.replace(" ", "")
@@ -115,3 +116,76 @@ def encrypt():
         else:
             print("{}{}".format(my_matrix[loc[0]][loc1[1]],my_matrix[loc1[0]][loc[1]]), end=' ')
         i=i+2
+
+
+# function to decrypt cipher text
+def decrypt():
+    # userKey.get() will return the value the user enters as a key
+    # It will get the value from the input field named userKey which had been defined above as a tkinter entry widget
+    key = userKey.get()
+    key = key.replace(" ", "")
+    key = key.upper()
+
+    ########################
+    def matrix(x, y, initial):
+        return [[initial for i in range(x)] for j in range(y)]
+
+    result = list()
+    for c in key:  # storing key
+        if c not in result:
+            if c == 'J':
+                result.append('I')
+            else:
+                result.append(c)
+
+    flag = 0
+    for i in range(65, 91):  # storing other character
+        if chr(i) not in result:
+            if i == 73 and chr(74) not in result:
+                result.append("I")
+                flag = 1
+            elif flag == 0 and i == 73 or i == 74:
+                pass
+            else:
+                result.append(chr(i))
+
+    k = 0
+    my_matrix = matrix(5, 5, 0)  # initialize matrix
+    for i in range(0, 5):  # making matrix
+        for j in range(0, 5):
+            my_matrix[i][j] = result[k]
+            k += 1
+
+    def locindex(c):  # get location of each character
+        loc = list()
+        if c == 'J':
+            c = 'I'
+        for i, j in enumerate(my_matrix):
+            for k, l in enumerate(j):
+                if c == l:
+                    loc.append(i)
+                    loc.append(k)
+                    return loc
+    ###################
+
+    # userText.get() will return the value the user enters as a plain text
+    # It will get the value from the input field named userText which had been defined above, as a tkinter entry widget
+    msg = userText.get()
+    msg = msg.upper()
+    msg = msg.replace(" ", "")
+    print("PLAIN TEXT: ",end=' ')
+    i=0
+    while i<len(msg):
+        loc=list()
+        loc=locindex(msg[i])
+        loc1=list()
+        loc1=locindex(msg[i+1])
+        if loc[1]==loc1[1]:
+            print("{}{}".format(my_matrix[(loc[0]-1)%5][loc[1]],my_matrix[(loc1[0]-1)%5][loc1[1]]),end=' ')
+        elif loc[0]==loc1[0]:
+            print("{}{}".format(my_matrix[loc[0]][(loc[1]-1)%5],my_matrix[loc1[0]][(loc1[1]-1)%5]),end=' ')
+        else:
+            print("{}{}".format(my_matrix[loc[0]][loc1[1]],my_matrix[loc1[0]][loc[1]]),end=' ')
+        i=i+2
+
+myWindow.mainloop()
